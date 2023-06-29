f1e50db0dd1d9987358e5f27413bb11fe327b402
Arsenii Vladimirov <78355358+grokhi@users.noreply.github.com>
2023-06-29 23:00:33 +0600

    Update settings.py

--- src/settings.py
+++ src/settings.py
@@ -22,7 +22,7 @@ RELEASE_YEAR: int = 2020
 HOMEPAGE_URL: str = "https://github.com/BinaLab/FloodNet-Challenge-EARTHVISION2021#floodnet-dataset"
 # e.g. "https://some.com/dataset/homepage"
 
-PREVIEW_IMAGE_ID: int = None
+PREVIEW_IMAGE_ID: int = 1385102
 # This should be filled AFTER uploading images to instance, just ID of any image.
 
 GITHUB_URL: str = "https://github.com/dataset-ninja/floodnet"
